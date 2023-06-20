0b10df570496d7932e828d730027c6f32a7bc19d
Василий <slaves.of.sun@gmail.com>
2023-06-20 23:28:24 +0300

    fix practicum tests

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
          CreateView.as_view(
             template_name='registration/registration_form.html',
             form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index')
-         ),
+            success_url=reverse_lazy('blog:index')),
          name='registration'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
